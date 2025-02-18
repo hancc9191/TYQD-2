494dd98a239d1b327fdb4dc108d04245e983b753
hancc9191 <105869123+hancc9191@users.noreply.github.com>
2025-02-18 11:04:17 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,1 @@
-module.exports = {
-  appToken:
-    process.env.WX_PUSHER_APP_TOKEN || "AT_FM1djM9wZ5mGdifCu0JDbXX7afEbb0xH",
-  uid: process.env.WX_PUSHER_UID || "UID_pvAfpsriissg6fIqRLl1K6SLCGTs",
-};
+
